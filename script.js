0117abf9a3b21741493a7f8e95789acbfc2a3437
Vinicius Brian Bolzani <90060747+VBrianB@users.noreply.github.com>
2022-07-23 10:24:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.logIN');
+
+const handleFocus = ({target}) =>{
+    const span = target.previousElementSibling;
+    span.classList.add('active')
+}
+const handleFocusOut = ({target}) =>{
+    if(target.value === '' ){
+    const span = target.previousElementSibling;
+    span.classList.remove('active')
+    }
+}
+const handleChange = () =>{
+    const [username, password] = inputs;
+    if(username.value && password.value.length > 8){
+        button.removeAttribute('disabled')
+    }
+    else{
+        button.setAttribute('disabled','')
+    }
+}
+inputs.forEach((input) => input.addEventListener('focus',handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout',handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input',handleChange));
